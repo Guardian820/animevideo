53bcfbdee1c6a5d39f36ec55dd317d09fc86c401
Malte N <malte.n@live.de>
2014-02-09 19:14:55 +0100

    some more improvements

--- src/FaceManga/MainBundle/Form/Type/AnimeType.php
+++ src/FaceManga/MainBundle/Form/Type/AnimeType.php
@@ -4,20 +4,46 @@ namespace FaceManga\MainBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class AnimeType extends AbstractType
 {
     
+    protected $trans;
+    
+    public function __construct(TranslatorInterface $trans)
+    {
+        $this->trans = $trans;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description', 'textarea')
-            ->add('year', 'number')
-            ->add('episodes', 'number')
+            ->add('name', null, array(
+                'attr' => array('placeholder' => $this->trans->trans('create_anime.placeholder.name'))
+            ))
+            ->add('description', 'textarea', array(
+                'attr' => array('placeholder' => $this->trans->trans('create_anime.placeholder.description'))
+            ))
+            ->add('year', 'number', array(
+                'attr' => array(
+                    'class' => 'number-spinner form-control',
+                    'min' => 1,
+                    'max' => 2014,
+                    'placeholder' => 2000
+                )
+            ))
+            ->add('episodes', 'number', array(
+                'attr' => array(
+                    'class' => 'number-spinner',
+                    'min' => 0,
+                    'max' => 999999,
+                    'placeholder' => 0
+                )
+            ))
             ->add('save', 'submit', array(
                 'attr' => array('class' => 'btn-primary'),
-                'label' => 'create_anime.save'
+                'label' => 'create_anime.placeholder.save'
             ));
     }
     
--- src/FaceManga/MainBundle/Resources/public/script.js
+++ src/FaceManga/MainBundle/Resources/public/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $('#create_anime_button').tooltip({});
+    
+    $('.number-spinner').each(function() {
+        $(this).TouchSpin({
+            min: $(this).attr('min'),
+            max: $(this).attr('max'),
+            step: 1
+        });
+    });
 });
