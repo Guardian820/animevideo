07173abb8752910a258fe72a791df96ca3ac8ba1
Malte N <malte.n@live.de>
2014-04-01 20:13:13 +0200

    Revert "web console"
    This reverts commit 546ed1750142513d085576556b6a9ff79697d37b.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,7 +24,6 @@ class AppKernel extends Kernel
             new Problematic\AclManagerBundle\ProblematicAclManagerBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle(),
-            new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
